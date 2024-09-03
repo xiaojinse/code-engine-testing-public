9e2c5d8b4fd7ab2875bab5681e9b5024783b134c
xiaojinse <xiaojinse@hotmail.com>
2024-09-03 17:18:04 +0900

    Create job.py

---
+++ jobs/job.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+import traceback
+import ibm_boto3
+import ibm_db
+from ibm_botocore.client import Config, ClientError
+from datetime import datetime, timedelta, timezone
+import re
+
+
+def main_job():
+    print("helen-testing-123")
+    
+main_job()
