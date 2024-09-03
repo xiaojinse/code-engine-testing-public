4c109f5d04bb8075612f446e1211512969dca876
xiaojinse <xiaojinse@hotmail.com>
2024-09-03 20:26:34 +0900

    Update __main__.py

--- functions_dependencies/__main__.py
+++ functions_dependencies/__main__.py
@@ -13,24 +13,12 @@ import re
 
 def main(params):
     print("helen-testing-functions-456")
-    
-    try:
-        read_xlsx_inst = Readxlsx(dict)
-        file_list = read_xlsx_inst.get_file_list()
-        read_xlsx_inst.exec_file_list(file_list)
-        print("Finished the program.")
-    except SystemExit as e:
-        print("Logging: Exit the program. As a testing.")
-        #return { 'message' : 'Returned: Exit the program. As a testing.'}
-    # return { 'message' : 'python implemention finished'}
-
-    name = params.get("name", "world")
-    greeting = "Hello " + name + "!"
+    greeting = "Hello Tester "
     return {
             "headers": {
                 "Content-Type": "application/json",
             },
             "statusCode": 200,
-            "body": greeting
+            "body": greeting,
     }
 
