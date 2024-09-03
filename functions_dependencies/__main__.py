1a23180f2d9837f15547654a4f00eacea1b4a5de
xiaojinse <xiaojinse@hotmail.com>
2024-09-03 19:57:17 +0900

    Update __main__.py

--- functions_dependencies/__main__.py
+++ functions_dependencies/__main__.py
@@ -13,5 +13,15 @@ import re
 
 
 def main():
-
     print("helen-testing-function-123")
+    name = params.get("name", "world")
+    greeting = "Hello " + name + "!"
+    
+    return {
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "statusCode": 200,
+        "body": greeting,
+    }
+
